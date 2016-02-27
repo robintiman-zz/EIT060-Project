6670821dc2673f063f83177c9463b485bf2bc711
Rob The Man <dat13rti@student.lu.se>
2016-02-27 18:43:58 +0100

    Added GUI

--- src/client/GUI.java
+++ src/client/GUI.java
@@ -3,5 +3,14 @@ package client;
 import javax.swing.JFrame;
 
 public class GUI extends JFrame {
+
+	public GUI() {
+		
+	}
+	
+	public void activate() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
